03df35a0acd7fc7368e2137a504523d4750bc960
Md_Waliul_Islam_Rayhan <b190305034@cse.jnu.ac.bd>
2023-05-21 18:40:22 +0600

    2.0

---
+++ src/Services/Catalog.API/Context/CatalogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoRepo.Context;
+
+namespace Catalog.API.Context
+{
+    public class CatalogDbContext : ApplicationDbContext
+    {
+        static IConfiguration configuration = new ConfigurationBuilder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true).Build();
+
+        public CatalogDbContext(string connectionString, string databaseName) : base(connectionString, databaseName)
+        {
+
+        }
+    }
+}
