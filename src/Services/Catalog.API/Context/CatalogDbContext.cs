7702f6b90aff4f571b5c80725fe925751ee83e73
Md_Waliul_Islam_Rayhan <b190305034@cse.jnu.ac.bd>
2023-05-22 00:20:45 +0600

    Repository Pattern Implement

--- src/Services/Catalog.API/Context/CatalogDbContext.cs
+++ src/Services/Catalog.API/Context/CatalogDbContext.cs
@@ -4,11 +4,11 @@ namespace Catalog.API.Context
 {
     public class CatalogDbContext : ApplicationDbContext
     {
-        static IConfiguration configuration = new ConfigurationBuilder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true).Build();
-
+        static IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+        static string connectionString = configuration.GetConnectionString("Catalog.API");
+        static string databaseName = configuration.GetValue<string>("DatabaseName");
         public CatalogDbContext(string connectionString, string databaseName) : base(connectionString, databaseName)
         {
-
         }
     }
 }
